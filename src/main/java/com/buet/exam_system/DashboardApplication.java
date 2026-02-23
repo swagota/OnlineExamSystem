e8d2581d043fba6610e3da4955756b5ac8d24dfd
tasneem <tasneembuet2025@gmail.com>
2026-02-23 18:28:26 +0600

    merging login and dashboard

--- src/main/java/com/buet/exam_system/DashboardApplication.java
+++ src/main/java/com/buet/exam_system/DashboardApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class DashboardApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(DashboardApplication.class.getResource("Dashboard.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(DashboardApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
         scene.getStylesheets().add(
                 getClass().getResource("/style.css").toExternalForm()
