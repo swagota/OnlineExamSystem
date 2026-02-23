34ae8ac167c93d72034fc0b9fc1a1969b40ca1f7
tasneem <tasneembuet2025@gmail.com>
2026-02-23 20:24:54 +0600

    teachers part

---
+++ src/main/java/com/buet/exam_system/teacherController.java
@@ -0,0 +1,12 @@
+package com.buet.exam_system;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class teacherController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    }
+}
